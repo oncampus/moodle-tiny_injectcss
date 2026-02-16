91508554e772a85e888857b9d3c7fcb0f50a5806
Marius Rosenbaum <marius.rosenbaum@oncampus.de>
2026-02-16 07:35:08 +0000

    upped version for release 4.5.0

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_injectcss';
 $plugin->release = '1.0.0';
-$plugin->version = 2025112800;
+$plugin->version = 2025112801;
 $plugin->requires = 2024100707;
 $plugin->maturity = MATURITY_STABLE;
